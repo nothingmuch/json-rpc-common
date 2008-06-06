541f17a10e00b67f0e0afac785f966e5c1453436
Yuval Kogman <nothingmuch@woobling.org>
2008-06-06 05:27:53 +0000

    boilerplate

--- lib/JSON/RPC/Common.pm
+++ lib/JSON/RPC/Common.pm
@@ -142,6 +142,21 @@ L<http://groups.google.com/group/json-rpc/web/json-rpc-over-http>
 
 =back
 
-=cut
+=head1 VERSION CONTROL
 
+This module is maintained using Darcs. You can get the latest version from
+L<http://nothingmuch.woobling.org/code>, and use C<darcs send> to commit
+changes.
+
+=head1 AUTHOR
+
+Yuval Kogman E<lt>nothingmuch@woobling.orgE<gt>
+
+=head1 COPYRIGHT
+
+	Copyright (c) 2008 Yuval Kogman. All rights reserved
+	This program is free software; you can redistribute
+	it and/or modify it under the same terms as Perl itself.
+
+=cut
 
