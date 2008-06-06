07601a04200a43ba3ba8ee56b0f407540f612e94
Yuval Kogman <nothingmuch@woobling.org>
2008-06-06 17:49:26 +0000

    relay errors to the user

--- examples/http_engine.pl
+++ examples/http_engine.pl
@@ -16,22 +16,31 @@ run {
 
 	# decode the request into an object
 	# this takes the JSON from the body, deserializes, and constructs a call
-	my $json_rpc_req = $marshal->request_to_call($c->req);
+	eval {
+		my $json_rpc_req = $marshal->request_to_call($c->req);
 
-	# fake some sort of return value without actually calling anything
-	my $json_rpc_res = $json_rpc_req->return_result({
-		time => scalar localtime,
-		echo => {
-			params => [ $json_rpc_req->params_list ],
-			method  => $json_rpc_req->method,
-		},
-	});
+		# fake some sort of return value without actually calling anything
+		my $json_rpc_res = $json_rpc_req->return_result({
+			time => scalar localtime,
+			echo => {
+				params => [ $json_rpc_req->params_list ],
+				method  => $json_rpc_req->method,
+			},
+		});
 
-	# to invoke the request as a method of some object:
-	# my $json_rpc_res = $json_rpc_req->call( $some_object );
+		warn "dancing";
 
-	# and write the result back to the HTTP client
+		# to invoke the request as a method of some object:
+		# my $json_rpc_res = $json_rpc_req->call( $some_object );
 
-	$marshal->write_result_to_response($json_rpc_res, $c->res);
+		# and write the result back to the HTTP client
+
+		$marshal->write_result_to_response($json_rpc_res, $c->res);
+	};
+
+	if ($@) {
+		$c->res->status(500);
+		$c->res->body($@);
+	}
 };
 
