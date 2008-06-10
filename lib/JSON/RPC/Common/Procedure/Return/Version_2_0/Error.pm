a84066799d9ec5bd467d944bc3c5757c07b5c896
Yuval Kogman <nothingmuch@woobling.org>
2008-06-10 12:19:31 +0000

    namespace::clean and immutable for 2.0 error

--- lib/JSON/RPC/Common/Procedure/Return/Version_2_0/Error.pm
+++ lib/JSON/RPC/Common/Procedure/Return/Version_2_0/Error.pm
@@ -3,8 +3,11 @@
 package JSON::RPC::Common::Procedure::Return::Version_2_0::Error;
 use Moose;
 
+use namespace::clean -except => [qw(meta)];
+
 extends qw(JSON::RPC::Common::Procedure::Return::Error);
 
+__PACKAGE__->meta->make_immutable;
 
 __PACKAGE__
 
