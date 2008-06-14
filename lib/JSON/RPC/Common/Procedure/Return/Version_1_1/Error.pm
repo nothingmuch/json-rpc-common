9e5f9332ae792245f1fb8108fd5ad190989b0c5d
Yuval Kogman <nothingmuch@woobling.org>
2008-06-14 21:23:34 +0000

    1.1 error class

--- lib/JSON/RPC/Common/Procedure/Return/Version_1_1/Error.pm
+++ lib/JSON/RPC/Common/Procedure/Return/Version_1_1/Error.pm
@@ -3,18 +3,35 @@
 package JSON::RPC::Common::Procedure::Return::Version_1_1::Error;
 use Moose;
 
+use JSON::RPC::Common::TypeConstraints qw(JSONValue);
+
 use namespace::clean -except => [qw(meta)];
 
 extends qw(JSON::RPC::Common::Procedure::Return::Error);
+
+has '+message' => (
+	required => 1,
+);
 
 has '+code' => (
 	required => 1,
 );
 
-has error => (
-	isa => "Any",
-	is  => "rw",
+has name => (
+	is      => "rw",
+	default => "JSONRPCError", # fucking idiots
 );
+
+sub deflate {
+	my $self = shift;
+
+	return {
+		name    => $self->name,
+		message => $self->message,
+		code    => $self->code,
+		( $self->has_data ? ( error => $self->data ) : () ),
+	},
+}
 
 __PACKAGE__->meta->make_immutable;
 
