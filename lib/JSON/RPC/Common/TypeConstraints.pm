c6bf4c353c3f894add019316f1db33dba6fbf20d
Yuval Kogman <nothingmuch@woobling.org>
2009-07-02 14:46:43 -0500

    fix MooseX::Types warnings

--- lib/JSON/RPC/Common/TypeConstraints.pm
+++ lib/JSON/RPC/Common/TypeConstraints.pm
@@ -6,12 +6,13 @@ use strict;
 use warnings;
 
 use MooseX::Types -declare => [qw(JSONDefined JSONValue JSONContainer)];
+use MooseX::Types::Moose qw(Value ArrayRef HashRef Undef);
 
-subtype JSONDefined, as "Value|ArrayRef|HashRef";
+subtype JSONDefined, as Value|ArrayRef|HashRef;
 
-subtype JSONValue, as 'Undef|Value|ArrayRef|HashRef';
+subtype JSONValue, as Undef|Value|ArrayRef|HashRef;
 
-subtype JSONContainer, as 'ArrayRef|HashRef';
+subtype JSONContainer, as ArrayRef|HashRef;
 
 __PACKAGE__
 
